943c4f6dd34c70fbef438ffa16435fca11cb8585
silviocdjm <81332335+silviocdjm@users.noreply.github.com>
2021-12-21 00:15:56 -0300

    Update WebinarIF

--- AvaliacaoTresPatterns3/src/Model/WebinarIF.java
+++ AvaliacaoTresPatterns3/src/Model/WebinarIF.java
@@ -1,7 +1,12 @@
 package Model;
 
 public interface WebinarIF {
-
+	
+	public String getId();
+	public String getTitle();
+	public double getPrice();
+	public void play();
+	public String getHoster();
+	public long getMinutes();
+	public boolean wasWatched();
 }
-
-
