3145fc009baab63119309f8a726e449f9f01c13d
silviocdjm <81332335+silviocdjm@users.noreply.github.com>
2021-12-21 00:13:10 -0300

    Update WebinarAdapter

--- AvaliacaoTresPatterns3/src/Model/WebinarAdapter.java
+++ AvaliacaoTresPatterns3/src/Model/WebinarAdapter.java
@@ -2,5 +2,39 @@ package Model;
 
 public class WebinarAdapter extends Disciplina{
 	
+	private Webinar webinar;
 
+	public WebinarAdapter(String title, String id, String hoster, double price, long minutes) {
+		super(title, id);
+		this.webinar = new Webinar(id, title, hoster, price, minutes);
+	}	
+	
+	@Override
+	public String getNome() {
+		return this.webinar.getTitle();
+	}
+	
+	@Override
+	public String getCodigo() {
+		return this.webinar.getId();
+	}
+		
+	@Override
+	public double getPreco() {
+		return this.webinar.getPrice();
+	}
+	
+	@Override
+	public double getChTotal() {
+		return this.webinar.getMinutes()/60;
+	}
+	
+	@Override
+	public double getPctCumprido() {
+		if(this.webinar.wasWatched()) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
 }
