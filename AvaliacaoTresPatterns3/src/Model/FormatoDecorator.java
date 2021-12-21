89e59ff28b54bd75b9b16f01d7cb48900a8c0076
silviocdjm <81332335+silviocdjm@users.noreply.github.com>
2021-12-21 00:08:09 -0300

    Update FormatoDecorator

--- AvaliacaoTresPatterns3/src/Model/FormatoDecorator.java
+++ AvaliacaoTresPatterns3/src/Model/FormatoDecorator.java
@@ -2,5 +2,20 @@ package Model;
 
 public class FormatoDecorator implements FormatoIF {
 
+	protected FormatoIF formatado;
+	
+	public FormatoDecorator(FormatoIF formatado) {
+		this.formatado = formatado;
+	}
+	
+	@Override
+	public double getPercentLivro() {		
+		return this.formatado.getPercentLivro();
+	}
+
+	@Override
+	public String getFormatacao() {		
+		return this.formatado.getFormatacao();
+	}
 
 }
